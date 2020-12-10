07a8d04c3fe4cf1906a817cc772bfac1d4403ac0
Matias Lopez Klyver <lopezk_matias@hotmail.com>
2020-12-10 01:41:44 -0300

    first changes made

--- client/src/components/OpenConversation.js
+++ client/src/components/OpenConversation.js
@@ -1,6 +1,9 @@
-import React, { useState, useCallback } from 'react'
-import { Form, InputGroup, Button } from 'react-bootstrap'
+import React, { useState, useCallback } from 'react';
+import { Form, InputGroup, Button } from 'react-bootstrap';
 import { useConversations } from '../contexts/ConversationsProvider';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircle, faEllipsisV, faGrinTongueWink, faMicrophone, faPaperclip, faPaperPlane, faPhoneAlt, faStopwatch } from '@fortawesome/free-solid-svg-icons';
+import '../Styles/Styles.css';
 
 export default function OpenConversation() {
   const [text, setText] = useState('')
@@ -23,10 +26,32 @@ export default function OpenConversation() {
 
   return (
     <div className="d-flex flex-column flex-grow-1">
+      <div className="d-flex justify-content-end p-4 align-items-center border-bottom border-left">
+        <div className="d-flex justify-content-between">
+          <FontAwesomeIcon icon={faStopwatch} className="mr-3"/>
+          <FontAwesomeIcon icon={faPhoneAlt} className="mr-3"/>
+        </div>
+        <div className="contact-picture mr-2"></div>
+        <div>Jessica Grill</div>
+      </div>
+      <div className="p-2 mr-2 ml-2 border-bottom d-flex flex-row justify-content-between align-items-center">
+        <div className="d-flex flex-row align-items-center">
+          <div className="contact-picture mr-2"></div>
+          <div>
+            <div className="small font-weight-bold">Lisa Westfall</div>
+              <div className="small-status d-flex align-items-center">
+                <FontAwesomeIcon icon={faCircle} className="small-icon mr-1"/>
+                <span> Active Now</span>
+              </div>
+          </div>
+        </div>
+        <FontAwesomeIcon icon={faEllipsisV} className="mr-3"/>
+      </div>
       <div className="flex-grow-1 overflow-auto">
         <div className="d-flex flex-column align-items-start justify-content-end px-3">
           {selectedConversation.messages.map((message, index) => {
             const lastMessage = selectedConversation.messages.length - 1 === index
+            console.log('mensaaajeeeee', message)
             return (
               <div
                 ref={lastMessage ? setRef : null}
@@ -34,7 +59,7 @@ export default function OpenConversation() {
                 className={`my-1 d-flex flex-column ${message.fromMe ? 'align-self-end align-items-end' : 'align-items-start'}`}
               >
                 <div
-                  className={`rounded px-2 py-1 ${message.fromMe ? 'bg-primary text-white' : 'border'}`}>
+                  className={`rounded px-2 py-1 ${message.fromMe ? 'chat-color text-black' : 'border'}`}>
                   {message.text}
                 </div>
                 <div className={`text-muted small ${message.fromMe ? 'text-right' : ''}`}>
@@ -48,19 +73,36 @@ export default function OpenConversation() {
       <Form onSubmit={handleSubmit}>
         <Form.Group className="m-2">
           <InputGroup>
+            <InputGroup.Prepend >
+              <InputGroup.Text>
+                <FontAwesomeIcon icon={faGrinTongueWink} />
+              </InputGroup.Text>
+            </InputGroup.Prepend>
             <Form.Control
               as="textarea"
               required
               value={text}
               onChange={e => setText(e.target.value)}
-              style={{ height: '75px', resize: 'none' }}
+              style={{ height: '40px', resize: 'none' }}
+              placeholder="Enter your message here..."
             />
-            <InputGroup.Append>
-              <Button type="submit">Send</Button>
+            <InputGroup.Append >
+              <InputGroup.Text>
+                <FontAwesomeIcon icon={faMicrophone} />
+              </InputGroup.Text>
             </InputGroup.Append>
+            <InputGroup.Append >
+              <InputGroup.Text className="rounded-right">
+                <FontAwesomeIcon icon={faPaperclip} />
+              </InputGroup.Text>
+            </InputGroup.Append>
+            <Button className="button-send ml-2" type="submit">
+              <FontAwesomeIcon icon={faPaperPlane} />
+            </Button>
           </InputGroup>
         </Form.Group>
       </Form>
     </div>
+  
   )
 }
--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'
-import { Tab, Nav, Button, Modal } from 'react-bootstrap'
+import { Tab, Nav, Button, Modal, Form, InputGroup, FormControl } from 'react-bootstrap'
 import Conversations from './Conversations'
 import Contacts from './Contacts'
 import NewContactModal from './NewContactModal'
 import NewConversationModal from './NewConversationModal'
+import '../Styles/Styles.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faCog, faPowerOff, faSearch } from '@fortawesome/free-solid-svg-icons';
 
 const CONVERSATIONS_KEY = 'conversations'
 const CONTACTS_KEY = 'contacts'
@@ -18,38 +21,60 @@ export default function Sidebar({ id }) {
   }
 
   return (
-    <div style={{ width: '250px' }} className="d-flex flex-column">
-      <Tab.Container activeKey={activeKey} onSelect={setActiveKey}>
-        <Nav variant="tabs" className="justify-content-center">
-          <Nav.Item>
-            <Nav.Link eventKey={CONVERSATIONS_KEY}>Conversations</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link eventKey={CONTACTS_KEY}>Contacts</Nav.Link>
-          </Nav.Item>
-        </Nav>
-        <Tab.Content className="border-right overflow-auto flex-grow-1">
-          <Tab.Pane eventKey={CONVERSATIONS_KEY}>
-            <Conversations />
-          </Tab.Pane>
-          <Tab.Pane eventKey={CONTACTS_KEY}>
-            <Contacts />
-          </Tab.Pane>
-        </Tab.Content>
-        <div className="p-2 border-top border-right small">
-          Your Id: <span className="text-muted">{id}</span>
+    <div className="d-flex flex-row">
+      <div className="d-flex flex-column sidebar-first-column"> 
+        <FontAwesomeIcon icon={faBars} className="navbar-open-menu" />
+        <div className="d-flex flex-column sidebar-bottom-icons">
+          <FontAwesomeIcon icon={faCog} className="navbar-open-menu" />
+          <FontAwesomeIcon icon={faPowerOff} className="navbar-open-menu" />
         </div>
-        <Button onClick={() => setModalOpen(true)} className="rounded-0">
-          New {conversationsOpen ? 'Conversation' : 'Contact'}
-        </Button>
-      </Tab.Container>
+      </div>
+      <div className="d-flex flex-column sidebar-second-column">
+        <span className="inbox-title" >Inbox</span>
+        <Form>
+          <InputGroup className="mb-2 search-bar">
+            <InputGroup.Prepend >
+              <InputGroup.Text>
+                <FontAwesomeIcon icon={faSearch} />
+              </InputGroup.Text>
+            </InputGroup.Prepend>
+            <FormControl id="inlineFormInputGroup" placeholder="Search"/>
+          </InputGroup>
+        </Form>
+      </div>
+      <div style={{ width: '250px' }} className="d-flex flex-column">
+        <Tab.Container activeKey={activeKey} onSelect={setActiveKey}>
+          <Nav variant="tabs" className="justify-content-center">
+            <Nav.Item>
+              <Nav.Link eventKey={CONVERSATIONS_KEY}>Conversations</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey={CONTACTS_KEY}>Contacts</Nav.Link>
+            </Nav.Item>
+          </Nav>
+          <Tab.Content className="border-right overflow-auto flex-grow-1">
+            <Tab.Pane eventKey={CONVERSATIONS_KEY}>
+              <Conversations />
+            </Tab.Pane>
+            <Tab.Pane eventKey={CONTACTS_KEY}>
+              <Contacts />
+            </Tab.Pane>
+          </Tab.Content>
+          <div className="p-2 border-top border-right small">
+            Your Id: <span className="text-muted">{id}</span>
+          </div>
+          <Button onClick={() => setModalOpen(true)} className="rounded-0">
+            New {conversationsOpen ? 'Conversation' : 'Contact'}
+          </Button>
+        </Tab.Container>
 
-      <Modal show={modalOpen} onHide={closeModal}>
-        {conversationsOpen ?
-          <NewConversationModal closeModal={closeModal} /> :
-          <NewContactModal closeModal={closeModal} />
-        }
-      </Modal>
+        <Modal show={modalOpen} onHide={closeModal}>
+          {conversationsOpen ?
+            <NewConversationModal closeModal={closeModal} /> :
+            <NewContactModal closeModal={closeModal} />
+          }
+        </Modal>
+      </div>
     </div>
   )
 }
